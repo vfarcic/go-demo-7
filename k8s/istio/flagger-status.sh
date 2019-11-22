130c964316de8a78931c976a1cb572e0983f56c9
Viktor Farcic <vfarcic@farcic.com>
2019-11-21 18:39:50 -0800

    Istio

---
+++ k8s/istio/flagger-status.sh
@@ -0,0 +1,31 @@
+ADDR=$1
+
+PROGRESSING=false
+
+EXIT_CODE=0
+
+while true; do 
+
+    curl -H "Host: go-demo-7.acme.com" "$ADDR"
+
+    STATUS=$(kubectl --namespace go-demo-7 \
+        get canary go-demo-7 \
+        --output jsonpath="{.status.phase}")
+
+    echo "Status: $STATUS"
+
+    if [[ "$PROGRESSING" == "false" && "$STATUS" == 'Progressing' ]]; then
+        PROGRESSING=true
+    elif [[ "$PROGRESSING" == "true" && "$STATUS" == 'Succeeded' ]]; then
+        echo "Canary deployment succeeded"
+        break
+    elif [[ "$PROGRESSING" == "true" && "$STATUS" == 'Failed' ]]; then
+        echo "Canary deployment failed"
+        break
+    fi
+
+    sleep 1
+
+done
+
+exit $EXIT_CODE
---
+++ k8s/istio/get-ingress-host.sh
@@ -0,0 +1,26 @@
+PROVIDER=$1
+
+if [[ "$PROVIDER" == "minikube" ]]; then
+    export INGRESS_PORT=$(kubectl \
+        --namespace istio-system \
+        get service istio-ingressgateway \
+        --output jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+    export INGRESS_HOST=$(minikube ip):$INGRESS_PORT
+elif [[ "$PROVIDER" == "docker" ]]; then
+    export INGRESS_HOST=127.0.0.1
+elif [[ "$PROVIDER" == "eks" ]]; then    
+    export INGRESS_HOSTNAME=$(kubectl \
+        --namespace istio-system \
+        get service istio-ingressgateway \
+        --output jsonpath="{.status.loadBalancer.ingress[0].hostname}")
+    export INGRESS_HOST=$(
+        dig +short $INGRESS_HOSTNAME \
+        | tail -n 1)
+else
+    export INGRESS_HOST=$(kubectl \
+        --namespace istio-system \
+        get service istio-ingressgateway \
+        --output jsonpath="{.status.loadBalancer.ingress[0].ip}")
+fi
+
+echo $INGRESS_HOST
